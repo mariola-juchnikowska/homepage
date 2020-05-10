38c95f470516e50d96da6076ab73b5d13a5b7bb9
Mariola Juchnikowska <mariola.oktaba@gmail.com>
2020-05-10 14:18:07 +0200

    DOM tasks and JS modified

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 alert('Takie a`la coockiesy ;)');
+
+const heading = document.querySelector('.page__content--js');
+console.log(heading);
+
+heading.innerHTML = ''; //zmieniamy to co jest w html i wstawiamy nową wersję.
